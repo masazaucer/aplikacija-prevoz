18e1da3046089a7f3dca592d5d8663f1c92a8c8c
Masa Zaucer <masa.zaucer@student.fmf.uni-lj.si>
2021-07-07 16:31:10 +0200

    ustvarjen razred pot

--- model.py
+++ model.py
@@ -17,3 +17,18 @@ class Uporabnik:
         elif vrednost == 'malo':
             self.pomembnost_onesnazevanja = False
 
+class Pot:
+    def __init__(self, zacetek, konec, cena):
+        self.zacetek = zacetek
+        self.konec = konec
+        self.cena = cena
+
+    def razdalja(self):
+        return self.konec - self.zacetek
+
+    def trajanje(self):
+        return None
+
+    def izracunaj_ceno(self):
+        self.cena += cena_na_km * self.razdalja()
+        return self.cena
