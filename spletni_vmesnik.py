81573fc8da7b0542f263e51a046cdca252eb2eb6
Masa Zaucer <masa.zaucer@student.fmf.uni-lj.si>
2021-08-14 19:13:02 +0200

    narejena osnovna stran, ki prikaže sredstva in poti

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,7 +1,15 @@
 import bottle
+from model import Stanje, Uporabnik, Prevozno_sredstvo, Pot
+
+DATOTEKA_S_STANJEM = "stanje.json"
+
+try:
+    stanje = Stanje.nalozi_stanje(DATOTEKA_S_STANJEM)
+except FileNotFoundError:
+    stanje = Stanje()
 
 @bottle.get("/")
 def osnovna_stran():
-    return bottle.template('osnovna_stran.tpl')
+    return bottle.template('osnovna_stran.tpl', sredstva=stanje.prevozna_sredstva, poti=stanje.poti)
 
 bottle.run(reloader=True, debug=True)
